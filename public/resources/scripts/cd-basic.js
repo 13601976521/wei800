8b1c5de6f10e144aedab19df6fd2221bfe7e273e
Dong Chen <cdcchen@gmail.com>
2012-11-19 22:28:43 +0800

    调整resources位置

--- public/resources/scripts/cd-basic.js
+++
@@ -1,9 +0,0 @@
-var CDBasic = {};
-CDBasic.urlValidate = function(url) {
-	var pattern = /http:\/\/[\w-]*(\.[\w-]*)+/ig;
-	return pattern.test(url);
-};
-CDBasic.emailValidate = function(email) {
-	var pattern = /^[\w-]+(\.[\w-]+)*@[\w-]+(\.[\w-]+)+$/ig;
-	return pattern.test(email);
-};
