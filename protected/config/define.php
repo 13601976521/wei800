58296375489eb34370f5e18337d2ec9ae9aec820
Dong Chen <cdcchen@gmail.com>
2012-11-21 00:00:45 +0800

    添加自定义配置

--- protected/config/define.php
+++ protected/config/define.php
@@ -5,16 +5,9 @@ define('CD_NO', 0);
 define('TABLE_POST', '{{post}}');
 define('TABLE_POST_WEIXIN', '{{post_weixin}}');
 define('TABLE_COMMENT', '{{comment}}');
-define('TABLE_CATEGORY', '{{category}}');
 define('TABLE_CONFIG', '{{config}}');
-define('TABLE_TAG', '{{tag}}');
 define('TABLE_WEIXIN', '{{weixin}}');
-define('TABLE_WEIXIN_TAG', '{{weixin_tag}}');
 define('TABLE_USER', '{{user}}');
-define('TABLE_USER_PROFILE', '{{user_profile}}');
-define('TABLE_LINK', '{{link}}');
-define('TABLE_ADVERT', '{{advert}}');
-define('TABLE_ADCODE', '{{adcode}}');
 define('TABLE_ADWEIXIN', '{{adweixin}}');
 
 /* comment state */
@@ -29,12 +22,6 @@ define('COMMENT_RATING_REPORT', 3);
 define('USER_STATE_UNVERIFY', 0);
 define('USER_STATE_ENABLED', 1);
 define('USER_STATE_FORBIDDEN', -1);
-/* advert state */
-define('ADVERT_STATE_DISABLED', 0);
-define('ADVERT_STATE_ENABLED', 1);
-/* advert state */
-define('ADCODE_STATE_DISABLED', 0);
-define('ADCODE_STATE_ENABLED', 1);
 /* weixin state */
 define('WEIXIN_STATE_DISABLED', 0);
 define('WEIXIN_STATE_ENABLED', 1);
