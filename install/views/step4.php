aab48c83711a4f4d8df535833c1ebf6f7adca124
Dong Chen <cdcchen@gmail.com>
2012-11-29 22:16:43 +0800

    个性安装成功后的提示

--- install/views/step4.php
+++ install/views/step4.php
@@ -3,6 +3,8 @@
 <div class="alert alert-block alert-success">
     <h3>Congratulations！系统安装成功!</h3>
     <ul>
+        <li>注意：请使用默认账号登录，然后修改账号和密码！</li>
+        <li>注意：安装结束后请删除install文件夹，以保证不会被别人恶意二次安装。</li>
         <li><a href="http://www.weixin800.com/static/about" target="_blank">查看微800介绍</a></li>
         <li><a href="http://www.weixin800.com/help/guide" target="_blank">微800新手入门</a></li>
         <li><a href="http://www.weixin800.com/help/trick" target="_blank">微800实用技巧</a></li>
