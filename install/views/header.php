e2c4c334df4bd2f0f54d682d26fee8ba2389e9b2
Dong Chen <cdcchen@gmail.com>
2012-11-30 19:52:29 +0800

    修改为html5声明

--- install/views/header.php
+++ install/views/header.php
@@ -1,7 +1,7 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!DOCTYPE html>
+<html>
 <head>
-    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <meta charset="utf-8" />
     <title>微800 安装向导</title>
     <link media="screen" rel="stylesheet" type="text/css" href="./res/bootstrap/css/bootstrap.min.css" />
     <link media="screen" rel="stylesheet" type="text/css" href="./res/cd-install.css" />
